db57f1a304c3e2433219049f253c059797912214
whatisaProCoder <debnathpritam2005@gmail.com>
2025-01-17 18:59:05 +0530

    add pritam

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,16 @@
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // put your setup code here, to run once:
+    int result = myFunction(2, 3);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+int myFunction(int x, int y) { return x + y; }
 
 // added sagar
+// hi, pritam
